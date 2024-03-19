055f399feb42186e55bba9bc2e6d4afd11dc8130
Артём Ульхов <u79607574710@gmail.com>
2024-03-19 16:25:08 +0700

    ef

--- TestUI/Views/Windows/MainWindow.xaml.cs
+++ TestUI/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
@@ -6,11 +7,31 @@ using TestUI.Services;
 
 namespace TestUI
 {
+    public class ElememntList
+    {
+        public Guid Guid { get; set; }
+
+        public List<string> Fields { get; }
+
+        // Add a property to represent the currently selected field
+        public string SelectedField { get; set; }
+
+        public ElememntList()
+        {
+            Guid = Guid.NewGuid();
+            Fields = new List<string>() { "Цикл", "Статус", "ID", "№ дела", "Истец", "Ответчик", "срок 1", "срок 2", "срок 3", "Судья", "Что требуется" };
+            // Initialize SelectedField to a default value, e.g., the first field
+            SelectedField = Fields[0];
+        }
+    }
+
+
+
+
     public partial class MainWindow : Window
     {
-        public ObservableCollection<string> Columns { get; } = new ObservableCollection<string>
+        public ObservableCollection<ElememntList> Columns { get; } = new ObservableCollection<ElememntList>
             {
-                "sss", "ddd", "dddddsffs"
             };
 
 
@@ -19,20 +40,15 @@ namespace TestUI
             InitializeComponent();
 
             DesignerListBox.ItemsSource = Columns;
-            
-    /*        a1.ItemsSource = Columns;
-            a2.ItemsSource = Columns;
-            a3.ItemsSource = Columns;
-            a4.ItemsSource = Columns;*/
 
             this.DataContext = this;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            string k = "dddddddddd";
+            ElememntList elememnt = new();
 
-            Columns.Add(k);
+            Columns.Add(elememnt);
         }
 
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
@@ -41,6 +57,19 @@ namespace TestUI
 
             List<string> strings  = UIHelper.GetSelectedStringsFromComboBoxes(comboBoxes);
         }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Find the parent StackPanel of the button that was clicked
+            var button = (Button)sender;
+            var stackPanel = (StackPanel)button.Parent;
+
+            // Find the parent container of the StackPanel
+            var parentContainer = stackPanel.Parent;
+
+        }
+
+
 
 
     }
