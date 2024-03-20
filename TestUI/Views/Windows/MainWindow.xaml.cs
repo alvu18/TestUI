2fac1f1c5f56de0529a63ea90e4eedf81ec03af1
Артём Ульхов <u79607574710@gmail.com>
2024-03-20 10:57:12 +0700

    сделал перетаксивание

--- TestUI/Views/Windows/MainWindow.xaml.cs
+++ TestUI/Views/Windows/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
+using System.Xml.Linq;
 using TestUI.Services;
 
 namespace TestUI
@@ -15,20 +16,45 @@ namespace TestUI
 
         public List<string> Fields { get; }
 
-        // Add a property to represent the currently selected field
         public string SelectedField { get; set; }
 
         public ElememntList()
         {
             Guid = Guid.NewGuid();
+
             Fields = new List<string>() { "Цикл", "Статус", "ID", "№ дела", "Истец", "Ответчик", "срок 1", "срок 2", "срок 3", "Судья", "Что требуется" };
-            // Initialize SelectedField to a default value, e.g., the first field
+
             SelectedField = Fields[0];
+        }
+    }
+
+    public static class ListExtensions
+    {
+        public static void Swap(this ObservableCollection<ElememntList> list, ElememntList element1, ElememntList element2)
+        {
+            int index1 = list.IndexOf(element1);
+
+            ElememntList element = list.FirstOrDefault(x=> x.Guid == element2.Guid);
+
+            int index2 = list.IndexOf(element);
+
+            if (index1 < 0 || index2 < 0)
+            {
+                throw new ArgumentException("Один или оба элемента не найдены в списке.");
+            }
+
+            ElememntList temp = list[index1];
+
+            list[index1] = list[index2];
+
+            list[index2] = temp;
         }
     }
 
     public partial class MainWindow : Window
     {
+        public ListViewItem swapItem = new ListViewItem();
+
         public ObservableCollection<ElememntList> Columns { get; } = new ObservableCollection<ElememntList>();
 
         public MainWindow()
@@ -72,5 +98,27 @@ namespace TestUI
             }
         }
 
+        private void UIelement_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DependencyObject originalSource = e.OriginalSource as DependencyObject;
+
+            swapItem = UIHelper.FindVisualParent<ListViewItem>(originalSource);
+        }
+
+        private void UIelement_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            DependencyObject originalSource = e.OriginalSource as DependencyObject;
+
+            ListViewItem MouseUpItem = UIHelper.FindVisualParent<ListViewItem>(originalSource) ;
+
+            var element1 = MouseUpItem.Content as ElememntList;
+
+            var element2 = swapItem.Content as ElememntList;
+
+            if (MouseUpItem != null)
+            {
+                ListExtensions.Swap(Columns, element1, element2);
+            }
+        }
     }
 }
